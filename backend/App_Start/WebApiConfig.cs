18031291479c45f5ac7b9ed274557c0fedc27899
gyenesadam <wgyenesadam@gmail.com>
2021-06-07 11:52:07 +0200

    Modellek felvitele

--- backend/App_Start/WebApiConfig.cs
+++ backend/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using System.Net.Http.Headers;
+using System.Web.Http.Cors;
 
 namespace backend
 {
@@ -21,6 +22,8 @@ namespace backend
                 defaults: new { id = RouteParameter.Optional }
             );
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
         }
     }
 }
---
+++ backend/Models/Tagok.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace backend.Models
+{
+    public class Tagok
+    {
+        public int TagID { get; set; }
+        public string Nev { get; set; }
+        public string SZIG { get; set; }
+        public int IR { get; set; }
+        public string Cim { get; set; }
+        public string Statusz { get; set; }
+
+    }
+}
