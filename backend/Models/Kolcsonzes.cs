ff31655cb70b47d07be69131920b44e0a614f32f
gyenesadam <wgyenesadam@gmail.com>
2021-06-07 11:55:35 +0200

    Model felvitel kész

---
+++ backend/Models/Kolcsonzes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace backend.Models
+{
+    public class Kolcsonzes
+    {
+        public int KolcsID { get; set; }
+        public int Tag { get; set; }
+        public string Kolcsdatuma { get; set; }
+        public string Visszadatum { get; set; }
+        public int Media { get; set; }
+     }
+}
---
+++ backend/Models/Media.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace backend.Models
+{
+    public class Media
+    {
+        public int mediaID { get; set; }
+        public string Mediatipus { get; set; }
+        public string Cim { get; set; }
+        public string Szerzo { get; set; }
+        
+    }
+}
