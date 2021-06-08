1f30ab96d1ed144f96c44dbf28d2e456347e46f4
gyenesadam <wgyenesadam@gmail.com>
2021-06-08 18:15:00 +0200

    ROUTING

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TagokComponent} from './tagok/tagok.component';
+import {MediaComponent} from './media/media.component';
+import {KolcsonzesComponent} from './kolcsonzes/kolcsonzes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'tagok',component:TagokComponent},
+  {path:'media',component:MediaComponent},
+  {path:'kolcsonzes',component:KolcsonzesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
