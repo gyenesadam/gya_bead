60c15133edd29a92d621a69213a148897587a1f4
gyenesadam <wgyenesadam@gmail.com>
2021-06-08 20:25:02 +0200

    Nem egy a MODAL!!!! Hiba: a ModalTitle és az <app-add-edit-tagok> argumentumai!!!!

--- frontend/src/app/tagok/show-tagok/show-tagok.component.ts
+++ frontend/src/app/tagok/show-tagok/show-tagok.component.ts
@@ -11,10 +11,32 @@ export class ShowTagokComponent implements OnInit {
   constructor(private service:SharedService) { }
 
   TagList:any=[];
+  //ModalTitle: string;
+  indithato:boolean=false;
+  tag:any;
 
   ngOnInit(): void {
     this.refreshTagokList();
   }
+
+addClick() {
+this.tag={
+  TagID:0,
+  Nev:"",
+  SZIG:"",
+  IR:"",
+  Varos:"",
+  Statusz:""
+  }
+//this.ModalTitle="Tag hozzáadása";
+this.indithato=true;
+}
+closeClick(){
+  this.indithato=false;
+  this.refreshTagokList();
+}
+
+
 
   refreshTagokList(){
     this.service.getTagok().subscribe(data=>{
